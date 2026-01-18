2fbfc71326ff0c89ea81f7076b0d493287a9cfb3
timoshay <exampleloal@gmail.com>
2026-01-18 20:59:15 +0500

    delete some files

--- fonts.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/RobotoMono.zip
-unzip RobotoMono.zip -d ~/.local/share/fonts
-rm RobotoMono.zip
-fc-cache -fv
